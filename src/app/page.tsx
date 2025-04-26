b9d8bc9ff5d45df12463be7935846eaafc5de77a
Kamil Szczepanik <kszczepanikworkspace@gmail.com>
2025-04-26 16:23:09 +0400

    fix: omitted eslint warning in useEffect on volume change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -42,6 +42,7 @@ export default function Home() {
       sound.unload();
       URL.revokeObjectURL(url);
     };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [audioFiles]);
 
   const handlePlayPause = () => {
